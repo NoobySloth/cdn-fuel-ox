8acd5840f3a9127de83230c88e55faa38e5f566d
NoobySloth <noobytubez@gmail.com>
2023-02-03 10:42:46 +0000

    refactor(server): ox context support

--- server/fuel_sv.lua
+++ server/fuel_sv.lua
@@ -29,39 +29,43 @@ RegisterNetEvent("cdn-fuel:server:OpenMenu", function(amount, inGasStation, hasW
 		if Config.RenewedPhonePayment and purchasetype == "bank" then
 			TriggerClientEvent("cdn-fuel:client:phone:PayForFuel", src, fuelamounttotal)
 		else
-			TriggerClientEvent('qb-menu:client:openMenu', src, {
-				{
-					header = Lang:t("menu_refuel_header"),
-					isMenuHeader = true,
-					icon = "fas fa-gas-pump",
-				},
-				{
-					header = "",
-					icon = "fas fa-info-circle",
-					isMenuHeader = true,
-					txt = Lang:t("menu_purchase_station_header_1")..total..Lang:t("menu_purchase_station_header_2") ,
-				},
-				{
-					header = Lang:t("menu_purchase_station_confirm_header"),
-					icon = "fas fa-check-circle",
-					txt = Lang:t("menu_refuel_accept"),
-					params = {
-						event = "cdn-fuel:client:RefuelVehicle",
-						args = {
-							fuelamounttotal = fuelamounttotal, 
-							purchasetype = purchasetype,
+			if Config.ox_framework then
+					TriggerClientEvent('cdn-fuel:client:OpenContextMenu', src, total, fuelamounttotal, purchasetype)
+			else
+				TriggerClientEvent('qb-menu:client:openMenu', src, {
+					{
+						header = Lang:t("menu_refuel_header"),
+						isMenuHeader = true,
+						icon = "fas fa-gas-pump",
+					},
+					{
+						header = "",
+						icon = "fas fa-info-circle",
+						isMenuHeader = true,
+						txt = Lang:t("menu_purchase_station_header_1")..total..Lang:t("menu_purchase_station_header_2") ,
+					},
+					{
+						header = Lang:t("menu_purchase_station_confirm_header"),
+						icon = "fas fa-check-circle",
+						txt = Lang:t("menu_refuel_accept"),
+						params = {
+							event = "cdn-fuel:client:RefuelVehicle",
+							args = {
+								fuelamounttotal = fuelamounttotal, 
+								purchasetype = purchasetype,
+							}
 						}
-					}
-				},
-				{
-					header = Lang:t("menu_header_close"),
-					txt = Lang:t("menu_refuel_cancel"), 
-					icon = "fas fa-times-circle",
-					params = {
-						event = "qb-menu:closeMenu",
-					}
-				},
-			})
+					},
+					{
+						header = Lang:t("menu_header_close"),
+						txt = Lang:t("menu_refuel_cancel"), 
+						icon = "fas fa-times-circle",
+						params = {
+							event = "qb-menu:closeMenu",
+						}
+					},
+				})
+			end
 		end
 	end
 end)
